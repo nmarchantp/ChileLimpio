87635c71d226d37310dcc9d813c73c1e2b214693
Nicole <ni.marchant@duocuc.cl>
2024-04-27 11:20:05 -0400

    1.2

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-var nombre = document.getElementById('nombre');
-var password = document.getElementById('password');
-document.getElementById('error')
-error.style.color='red';
+// var nombre = document.getElementById('nombre');
+// var password = document.getElementById('password');
+// document.getElementById('error')
+// error.style.color='red';
 
-// function enviarFormulario(){
-//     console.log('Enviando Formulario...')
-//     var mensajesError = [];
+// // function enviarFormulario(){
+// //     console.log('Enviando Formulario...')
+// //     var mensajesError = [];
+
+// //     if(nombre.value === null || nombre.value === ''){
+// //         mensajesError.push('Ingresa tu nombre');
+// //     }
+// //     if(password.value === null || password.value === ''){
+// //         mensajesError.push('Ingresa tu password');
+// //     }
+
+// //     error.innerHTML = mensajesError.join(', ');
+// //     return false;
+// // }
+
+// var form = document.getElementById('formulario');
+//     form.addEventListener('submit',function(evt){
+//         evt.preventDefault();
+//         var mensajesError = [];
 
 //     if(nombre.value === null || nombre.value === ''){
 //         mensajesError.push('Ingresa tu nombre');
@@ -15,20 +31,5 @@ error.style.color='red';
 //     }
 
 //     error.innerHTML = mensajesError.join(', ');
-//     return false;
-// }
+//     });
 
-var form = document.getElementById('formulario');
-    form.addEventListener('submit',function(evt){
-        evt.preventDefault();
-        var mensajesError = [];
-
-    if(nombre.value === null || nombre.value === ''){
-        mensajesError.push('Ingresa tu nombre');
-    }
-    if(password.value === null || password.value === ''){
-        mensajesError.push('Ingresa tu password');
-    }
-
-    error.innerHTML = mensajesError.join(', ');
-    });
